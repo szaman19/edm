675e2bf606b10f57c34d70d3ecf672b0855ba092
Shehtab Zaman <szaman5@binghamton.edu>
2025-09-09 16:03:47 -0400

    Add CIGNWrapper class for image generation
    Implemented CIGNWrapper class for image generation with configurable parameters.

--- training/networks.py
+++ training/networks.py
@@ -671,3 +671,42 @@ class EDMPrecond(torch.nn.Module):
         return torch.as_tensor(sigma)
 
 #----------------------------------------------------------------------------
+
+@persistence.persistent_class
+class CIGNWrapper(torch.nn.Module):
+    def __init__(self,
+        img_resolution,                     # Image resolution.
+        img_channels,                       # Number of color channels.
+        label_dim       = 0,                # Number of class labels, 0 = unconditional.
+        use_fp16        = False,            # Execute the underlying model at FP16 precision?
+        sigma_min       = 0,                # Minimum supported noise level.
+        sigma_max       = float('inf'),     # Maximum supported noise level.
+        sigma_data      = 0.5,              # Expected standard deviation of the training data.
+        model_type      = 'DhariwalUNet',   # Class name of the underlying model.
+        **model_kwargs,                     # Keyword arguments for the underlying model.
+    ):
+        super().__init__()
+        self.img_resolution = img_resolution
+        self.img_channels = img_channels
+        self.label_dim = label_dim
+        self.use_fp16 = use_fp16
+        self.sigma_min = sigma_min
+        self.sigma_max = sigma_max
+        self.sigma_data = sigma_data
+        self.model = globals()[model_type](img_resolution=img_resolution, in_channels=img_channels, out_channels=img_channels, label_dim=label_dim, **model_kwargs)
+
+    def forward(self, x, sigma=None, class_labels=None, force_fp32=False, **model_kwargs):
+        x = x.to(torch.float32)
+        # sigma is actually c_noise = sigma.log() / 4
+        if sigma is None:
+            sigma = torch.Tensor([80]).squeeze(0).to(x.device)
+            sigma = sigma.log() / 4
+        sigma = sigma.to(torch.float32).reshape(-1, 1, 1, 1)
+        class_labels = None if self.label_dim == 0 else torch.zeros([1, self.label_dim], device=x.device) if class_labels is None else class_labels.to(torch.float32).reshape(-1, self.label_dim)
+        dtype = torch.float16 if (self.use_fp16 and not force_fp32 and x.device.type == 'cuda') else torch.float32
+
+        F_x = self.model(x.to(dtype), sigma.flatten(), class_labels=class_labels, **model_kwargs)
+        return F_x
+
+    def round_sigma(self, sigma):
+        return torch.as_tensor(sigma)
